301634f76ce19c4897c162e997123c8f8606917a
NilesDavis <nils@haferkemper.de>
2026-09-20 12:54:40 +0200

    Simplify comment on options reload behavior

--- custom_components/nanobeemespro_reader/__init__.py
+++ custom_components/nanobeemespro_reader/__init__.py
@@ -30,7 +30,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    # Reload integration when options (host/interval/factors) are changed
+    # Reload integration when options are changed
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
 
     return True
